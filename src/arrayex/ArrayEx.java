631acbe03a0232f8cd30ec6043e0311a17bc9802
nguye256 <nguye256@sheridancollege.ca>
2019-01-23 10:46:02 -0600

    Week 3 check

---
+++ src/arrayex/ArrayEx.java
@@ -0,0 +1,32 @@
+public class ArrayEx {
+	private String Name; //String variable
+	private int regNum;
+
+
+	public String getName() {				//Getter method
+		return Name;
+	}
+
+	public int getregNum() {
+		return regNum;
+	}
+
+	//constructor
+	public ArrayEx(String name, int no) {
+		this.Name = name;
+		this.regNum = no;
+	} //end of constructor
+
+
+	/*public void setName(String Name) {		//Setter method
+		this.Name = Name;
+	}*/
+
+	public static void main (String[] args) {
+		/*ArrayEx s1 = new ArrayEx();		//
+		s1.Name = "PooPoo";				//
+		System.out.println(s1.Name);*/
+	}
+
+}
+
